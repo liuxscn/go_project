f717574625451f14698a363dc303c828b35a212c
Sean L <emlliuxiang@126.com>
2018-08-30 22:03:57 +0800

    indie 1 commit: sync code from indie

--- rango/urls.py
+++ rango/urls.py
@@ -16,16 +16,43 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
 
 def login(request):
-    return render(request, 'login.html')
 
-def info(request):
-    pass
+    # return HttpResponse(request.method + ' hey there')
+    # return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        u = request.POST.get('user')
+        p = request.POST.get('pwd')
+        if u == 'root' and p == 'root':
+            return redirect('/index/')
+            # return redirect('https://github.com')
+        else:
+            return render(request, 'login.html', {'error_msg' : '用户名或密码错误'})
+
+def index(request):
+
+    return render(
+        request,
+        'index.html',
+        {
+            'name' : 'alex',
+            'users' : ['立志', '理解'],
+            'user_dict': {'k1' : 'v1', 'k2' : 'v2'},
+            'user_list_dict' : [
+                {'id':'1', 'name':'alex', 'email':'alex@qq.com'},
+                {'id':'2', 'name':'bran', 'email':'bran@qq.com'},
+                {'id':'3', 'name':'chan', 'email':'chan@qq.com'},
+            ]
+        }
+    )
+    # return HttpResponse(request.method + ' Index')
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^login/', login),
-    url(r'^info/', info),
+    url(r'^index/', index),
 ]
